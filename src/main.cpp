a76454a427be02ef4d512c6381faed1d4dbfd061
Mohamed lamine Fadiga <37674141-mohamedlaminef3@users.noreply.replit.com>
2024-11-29 10:09:01 +0000

    gestion du choix par un switch case

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,37 @@ using namespace std;
 
 int main() {
   std::cout << "Menu véhicule \n";
-  std::cout << "1 - Multiméia \n";
+  std::cout << "1 - Multimédia \n";
   std::cout << "2 - Téléphone \n";
   std::cout << "3 - Connexions \n";
   std::cout << "4 - Personalisation - Configuaration \n";
 
-  int choix;
+int choix;
+  
   std::cout << "Votre choix ? \n";
   std::cin >> choix;
+
+  switch (choix)
+    { 
+    case 1:
+    cout << "1 - Cartes \n";
+     break;
+    case 2:
+    cout << "2 - Musique\n";
+      break;
+    case 3:
+    cout << "3 - Connexion disponible\n ";
+      break; 
+    case 4:
+    cout << "4 - couleur\n ";
+      break;
+      
+    default: 
+    cout << "Traitement du cas de l’option invalide par un message d’erreur\n";
+      }
+  /* 
+
+
   
   if (choix == 1) {
     std::cout << "1 - Cartes \n";
@@ -31,4 +54,5 @@ int main() {
     std::cout
         << "Traitement du cas de l’option invalide par un message d’erreur \n ";
   }
+*/
 }
